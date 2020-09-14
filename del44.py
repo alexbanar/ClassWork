0e2995f48763ae405209b63052721b2c49ca9b11
alexbanar <alex.banar10@gmail.com>
2020-09-14 18:56:00 +0300

    Initial commit

---
+++ del44.py
@@ -0,0 +1,14 @@
+import pymysql
+
+# Establishing a connection to DB
+conn = pymysql.connect(host='localhost', port=3306, user='root', passwd='zagadaga369', db='users')
+conn.autocommit(True)
+
+# Getting a cursor from Database
+cursor = conn.cursor()
+
+# Deleting data into table
+cursor.execute("DELETE FROM users.users WHERE name = 'john'")
+
+cursor.close()
+conn.close()
---
+++ sel11.py
@@ -0,0 +1,17 @@
+import pymysql
+
+# Establishing a connection to DB
+conn = pymysql.connect(host='localhost', port=3306, user='root', passwd='zagadaga369', db='users')
+
+# Getting a cursor from Database
+cursor = conn.cursor()
+
+# Getting all data from table “users”
+cursor.execute("SELECT * FROM users.users;")
+
+# Iterating table and printing all users
+for row in cursor:
+    print(row)
+
+cursor.close()
+conn.close()
